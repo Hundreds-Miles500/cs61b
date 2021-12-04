7e6dfab7c27ba7fc001385666f3683c1d2f4acca
Aaron <zhangjiayi2002@outlook.com>
2021-12-05 02:33:29 +0800

    the first version of proj1a

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,107 @@
+import java.util.List;
+
+
+public class LinkedListDeque<T> {
+    private int size;
+    private IntNode sentinel;
+    //DLList default constructor
+    public LinkedListDeque(){
+        size = 0;
+        sentinel = new IntNode(sentinel, null, sentinel);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+    }
+    //copy the LinkedList
+    public LinkedListDeque(LinkedListDeque other){
+        this();
+        IntNode ptr = other.sentinel.next;
+        while(this.size != other.size){
+            this.addLast((T)ptr.item);
+            ptr = ptr.next;
+        }
+    }
+
+
+    //class IntNode
+    public class IntNode{
+        public IntNode prev;
+        public T item;
+        public IntNode next;
+
+        public IntNode(IntNode p, T i, IntNode n){
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    //method
+    public void addFirst(T item){
+        sentinel.next = new IntNode(sentinel, item, sentinel.next);
+        sentinel.next.next.prev = sentinel.next;
+        size += 1;
+    }
+
+    public void addLast(T item){
+        sentinel.prev = new IntNode(sentinel.prev, item, sentinel);
+        sentinel.prev.prev.next = sentinel.prev;
+        size += 1;
+    }
+
+    public boolean isEmpty(){
+        if (sentinel.next.item == null){
+            return true;
+        }
+        return false;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        IntNode ptr = sentinel.next;
+        while (ptr.item != null){
+            System.out.print(ptr.item + " ");
+            ptr = ptr.next;
+        }
+        System.out.println(" ");
+    }
+
+    public T removeFirst(){
+        IntNode ptr = sentinel.next;
+        if (ptr.item == null){
+            return null;
+        }
+        sentinel.next = ptr.next;
+        ptr.next.prev = sentinel;
+        ptr.next = null;
+        ptr.prev = null;
+        return ptr.item;
+    }
+
+    public T removeLast(){
+        IntNode ptr = sentinel.prev;
+        if (ptr.item == null){
+            return null;
+        }
+        sentinel.prev = ptr.prev;
+        ptr.prev.next = sentinel;
+        ptr.next = null;
+        ptr.prev = null;
+        return ptr.item;
+    }
+
+    public T get(int index){
+        IntNode ptr = sentinel.next;
+        if (ptr.next == null){
+            return null;
+        }
+        while (index != 0){
+            ptr = ptr.next;
+            index --;
+        }
+        return ptr.item;
+    }
+
+}
